a02f4a2c6995c037fb448e196b86b72f940429bd
EmperorWadada <emperor.dada@gmail.com>
2023-03-24 10:22:29 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Abdulshakur ismail ALANKO</p>
+        <div>We are moving the world of nations</div>
+        <form>
+          <p>name:</p>
+          <input type="text" />
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
